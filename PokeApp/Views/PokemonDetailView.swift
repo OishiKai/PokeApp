0843fe73790506532ff49a6c85c67fe020d26ff1
Kai Oishi <kai.appdeveloper@gmail.com>
2025-04-29 18:07:20 +0900

    add AudioKit

--- PokeApp/Views/PokemonDetailView.swift
+++ PokeApp/Views/PokemonDetailView.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 import AVFoundation
-import OggDecoder
 
 struct PokemonDetailView: View {
     let pokemon: Pokemon
@@ -103,39 +102,7 @@ struct PokemonDetailView: View {
     }
     
     private func playCry(url: String) {
-        // URLをそのまま使用
-        guard let audioUrl = URL(string: url) else { return }
-        
-        isConverting = true
-        
-        // 一時ファイルのURLを作成
-        let tempDir = FileManager.default.temporaryDirectory
-        let oggUrl = tempDir.appendingPathComponent("\(pokemon.id)_cry.ogg")
-        let wavUrl = tempDir.appendingPathComponent("\(pokemon.id)_cry.wav")
-        
-        // URLからデータをダウンロード
-        Task {
-            do {
-                let (data, _) = try await URLSession.shared.data(from: audioUrl)
-                try data.write(to: oggUrl)
-                
-                // OggDecoderを使用して変換
-                let decoder = OGGDecoder()
-                
-                await decoder.decode(oggUrl, into: wavUrl)
-                player = AVPlayer(url: wavUrl)
-                player?.play()
-                
-                // 再生が終了したら再生状態をリセット
-                NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player?.currentItem, queue: .main) { _ in
-                    isPlaying = false
-                }
-                
-                isConverting = false
-            } catch {
-                print("Failed to download ogg file: \(error)")
-                isConverting = false
-            }
-        }
+        // 一時的に機能を無効化
+        print("鳴き声再生機能は現在利用できません")
     }
 }
